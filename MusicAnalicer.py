4871455eeb018d5fa48283cd502f096fd4d2debb
Askepios <askepios@gmail.com>
2026-01-31 17:02:47 -0500

    Primer boceto

--- MusicAnalicer.py
+++ MusicAnalicer.py
@@ -1,3 +1,25 @@
 def analice():
-    input("Cual es su genero favotito?")
+    genero = input("Cual es su genero favotito?").lower()
+
+    generos = {
+        "progresivo": "Pink Floyd, Yes, Genesis, King Crimson, The Mars Volta",
+        "rock": "Queen, Nirvana, Guns N' Roses, The Beatles",
+        "pop": "Taylor Swift, Ariana Grande, Dua Lipa, Ed Sheeran",
+        "hip hop": "Kendrick Lamar, Drake, J. Cole, Travis Scott",
+        "jazz": "Miles Davis, John Coltrane, Ella Fitzgerald, Louis Armstrong",
+        "classical": "Ludwig van Beethoven, Wolfgang Amadeus Mozart, Johann Sebastian Bach, Frédéric Chopin"
+    }
+    
+    if genero in generos:
+        print("Te recomiendo esto:")
+        for artista in generos[genero]:
+            print("- " + artista)
+    else:
+        print("Lo siento, no tengo recomendaciones para ese género.")
+
+analice()
+
+
+
+
 
